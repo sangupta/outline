bdda1739dec2589889f5c6c16dc37c5d3d3936bf
Sandeep Gupta <sandy.pec@gmail.com>
2016-06-20 14:21:36 +0530

    Updated javadoc

--- src/main/java/com/sangupta/outline/util/OutlineUtil.java
+++ src/main/java/com/sangupta/outline/util/OutlineUtil.java
@@ -5,6 +5,14 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import com.sangupta.outline.Outline;
+
+/**
+ * Utility classes for {@link Outline} framework.
+ * 
+ * @author sangupta
+ *
+ */
 public class OutlineUtil {
     
     public static List<Field> getAllFields(Class<?> clazz) {
